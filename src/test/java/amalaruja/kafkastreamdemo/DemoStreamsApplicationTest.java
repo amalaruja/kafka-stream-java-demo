038c08c4eade4a2f85e1039112bc14a5908efdc1
Amal Aruja <amal.a.aux@go-jek.com>
2020-05-09 13:03:21 +0530

    Sets up gradle

---
+++ src/test/java/amalaruja/kafkastreamdemo/DemoStreamsApplicationTest.java
@@ -0,0 +1,10 @@
+package amalaruja.kafkastreamdemo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DemoStreamsApplicationTest {
+    @Test public void testAppHasAGreeting() {
+        DemoStreamsApplication classUnderTest = new DemoStreamsApplication();
+    }
+}
