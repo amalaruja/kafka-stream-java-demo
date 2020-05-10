4ad580b0518d6ccb519048d8cb375c2079aafff8
Amal Aruja <amal.a.aux@go-jek.com>
2020-05-10 07:38:19 +0530

    Adds unit test for case conversion topology

--- src/main/java/amalaruja/kafkastreamdemo/DemoStreamsApplication.java
+++ src/main/java/amalaruja/kafkastreamdemo/DemoStreamsApplication.java
@@ -9,6 +9,7 @@ import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
+
 class DemoStreamsApplication {
     public static void main(String args[]) {
         Properties props = new Properties();
@@ -17,16 +18,10 @@ class DemoStreamsApplication {
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        final StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> source = builder.stream("lowercase");
-
-        source.mapValues(str -> str.toUpperCase())
-            .to("uppercase");
-
-        final Topology topology = builder.build();
+        final Topology topology = createCaseConversionTopology("lowercase", "uppercase");
         final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
- 
+
         Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
             @Override
             public void run() {
@@ -34,7 +29,7 @@ class DemoStreamsApplication {
                 latch.countDown();
             }
         });
- 
+
         try {
             streams.start();
             latch.await();
@@ -42,5 +37,15 @@ class DemoStreamsApplication {
             System.exit(1);
         }
         System.exit(0);
+    }
+
+    public static Topology createCaseConversionTopology(String inputTopic, String outputTopic) {
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream(inputTopic);
+
+        source.mapValues(str -> str.toUpperCase())
+                .to(outputTopic);
+
+        return builder.build();
     }
 }
--- src/test/java/amalaruja/kafkastreamdemo/DemoStreamsApplicationTest.java
+++ src/test/java/amalaruja/kafkastreamdemo/DemoStreamsApplicationTest.java
@@ -1,10 +1,33 @@
 package amalaruja.kafkastreamdemo;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class DemoStreamsApplicationTest {
-    @Test public void testAppHasAGreeting() {
-        DemoStreamsApplication classUnderTest = new DemoStreamsApplication();
+    @Test
+    public void testCaseConversionTopology() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "demo-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final String inputTopicName = "lowercase", outputTopicName = "uppercase";
+        final Topology topology = DemoStreamsApplication.createCaseConversionTopology(inputTopicName, outputTopicName);
+        TopologyTestDriver testDriver = new TopologyTestDriver(topology, props);
+
+        TestInputTopic<String, String> inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.String().serializer(), Serdes.String().serializer());
+        TestOutputTopic<String, String> outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.String().deserializer(), Serdes.String().deserializer());
+
+        inputTopic.pipeInput("key", "hello");
+        assertThat(outputTopic.readKeyValue(), equalTo(new KeyValue<>("key", "HELLO")));
+
+        testDriver.close();
     }
 }
